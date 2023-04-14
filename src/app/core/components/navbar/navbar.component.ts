4379161327d781db6a561ba177bba1dd168d4a06
Hanss Espinoza <hanssabinadie@gmail.com>
2023-04-13 18:58:10 -0600

    feat: :sparkles: add navbar

---
+++ src/app/core/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Menu } from '../../models';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+  public menu: Menu[] = [];
+
+  constructor() {
+    this.menu = [
+      {
+        name: 'Inicio',
+        url: '/'
+      },
+      {
+        name: 'People',
+        url: '/people'
+      },
+      {
+        name: 'Films',
+        url: '/films'
+      },
+    ]
+  }
+
+}
